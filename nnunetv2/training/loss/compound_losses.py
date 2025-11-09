a38bdb9152a899713dcf2d84c16a5402a0aabd60
alvinkimbowa <kimbowa2alvin@gmail.com>
2025-11-09 14:21:27 -0800

    feat: Add DC_and_CE_Partial_loss and nnUNetTrainerPartialLoss classes
    - Introduced DC_and_CE_Partial_loss for improved loss calculation by supervising only relevant target areas.
    - Added nnUNetTrainerPartialLoss to integrate the new loss function with deep supervision capabilities, enhancing model training flexibility.

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -56,6 +56,29 @@ class DC_and_CE_loss(nn.Module):
         return result
 
 
+class DC_and_CE_Partial_loss(DC_and_CE_loss):
+    def forward(self, net_output: torch.Tensor, target: torch.Tensor):
+        """
+        target must be b, c, x, y(, z) with c=1
+        :param net_output:
+        :param target:
+        :return:
+        """
+        mask = target != 0 # only supervise where there is a target
+        # remove ignore label from target, replace with one of the known labels. It doesn't matter because we
+        # ignore gradients in those areas anyway
+        target_dice = torch.where(mask, target, 0)
+        num_fg = mask.sum()
+
+        dc_loss = self.dc(net_output, target_dice, loss_mask=mask) \
+            if self.weight_dice != 0 else 0
+        ce_loss = self.ce(net_output, target[:, 0]) \
+            if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
+
+        result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
+        return result
+
+
 class DC_and_CE_and_Edge_loss(DC_and_CE_loss):
     def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice=1, weight_edge=0.1, ignore_label=None,
                  dice_class=SoftDiceLoss):
---
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerPartialLoss.py
@@ -0,0 +1,37 @@
+import torch
+from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.training.loss.compound_losses import DC_and_CE_Partial_loss
+from nnunetv2.training.loss.dice import get_tp_fp_fn_tn, MemoryEfficientSoftDiceLoss
+import numpy as np
+
+
+class nnUNetTrainerPartialLoss(nnUNetTrainer):
+    def _build_loss(self):
+        loss = DC_and_CE_Partial_loss({'batch_dice': self.configuration_manager.batch_dice,
+                                'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
+                                ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
+
+        if self._do_i_compile():
+            loss.dc = torch.compile(loss.dc)
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+
+        if self.enable_deep_supervision:
+            deep_supervision_scales = self._get_deep_supervision_scales()
+            weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+            if self.is_ddp and not self._do_i_compile():
+                # very strange and stupid interaction. DDP crashes and complains about unused parameters due to
+                # weights[-1] = 0. Interestingly this crash doesn't happen with torch.compile enabled. Strange stuff.
+                # Anywho, the simple fix is to set a very low weight to this.
+                weights[-1] = 1e-6
+            else:
+                weights[-1] = 0
+
+            # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+            weights = weights / weights.sum()
+            # now wrap the loss
+            loss = DeepSupervisionWrapper(loss, weights)
+
+        return loss
