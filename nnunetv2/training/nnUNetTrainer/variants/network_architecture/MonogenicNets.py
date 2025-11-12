8fba58f9c0350b5fb4e5b17e90fe24a8c320a339
Alvin <kimbowa2alvin@gmail.com>
2025-11-11 18:27:23 -0800

    feat: Add new Monogenic UNet variants with enhanced functionality
    - Introduced MonoEnhancedUNet, MonoGatedUNet, MonoSkipFusionUNet, and MonoGatedSkipUNet classes, each incorporating the Mono2D layer for improved feature extraction and structural information enhancement.
    - Enhanced forward methods to utilize monogenic features for better image processing capabilities.
    - Added detailed docstrings for each new class to clarify their functionality and intended use.

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/MonogenicNets.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/MonogenicNets.py
@@ -15,3 +15,35 @@ class MonoUNet(ResidualEncoderUNet):
         return super().forward(x)
 
 
+class MonoEnhancedUNet(ResidualEncoderUNet):
+    """
+    ResidualEncoderUNet with Monogenic layer at the front-end (before encoder).
+    The layer extracts the local phase features and combines them with the original image
+    to enhance structural information (e.g. edges, blobs, lines, etc.) without losing 
+    intensity information.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mono2d = Mono2D()
+        print("\n\nUsing MonoEnhancedUNet with mono2d layer\n\n")
+    
+    def forward(self, x):
+        x = x + self.mono2d(x)
+        return super().forward(x)
+
+
+class MonoGatedUNet(ResidualEncoderUNet):
+    """
+    ResidualEncoderUNet with Monogenic layer at the front-end (before encoder).
+    The layer extracts the local phase features and uses them to gate the original image
+    to emphasize structural information (e.g. edges, blobs, lines, etc.).
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mono2d = Mono2D()
+        print("\n\nUsing MonoGatedUNet with mono2d layer\n\n")
+    
+    def forward(self, x):
+        x = x * self.mono2d(x)
+        return super().forward(x)
+
