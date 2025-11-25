144f330c5eb8014907ffac29af796c48be766a2f
Alvin <kimbowa2alvin@gmail.com>
2025-11-25 15:20:14 -0800

    feat: add CMUNeXt baseline trainers that also have deep supervision

---
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/CMUNeXtTrainer.py
@@ -0,0 +1,220 @@
+import torch
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from typing import Union, Tuple, List
+from torch import nn
+from nnunetv2.training.nnUNetTrainer.variants.network_architecture.unext.archs import UNext
+from torch._dynamo import OptimizedModule
+from nnunetv2.utilities.label_handling.label_handling import determine_num_input_channels
+from nnunetv2.training.dataloading.nnunet_dataset import infer_dataset_class
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
+class CMUNeXtTrainer(nnUNetTrainer):
+    @staticmethod
+    def build_network_architecture(architecture_class_name: str,
+                                   arch_init_kwargs: dict,
+                                   arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
+                                   num_input_channels: int,
+                                   num_output_channels: int,
+                                   patch_size: List[int],
+                                   enable_deep_supervision: bool = True) -> nn.Module:
+
+        return CMUNeXt(
+            input_channel=num_input_channels,
+            num_classes=num_output_channels,
+            dims=[16, 32, 128, 160, 256],
+            depths=[1, 1, 1, 3, 1],
+            kernels=[3, 3, 7, 7, 7]
+        )
+    
+    def set_deep_supervision_enabled(self, enabled: bool):
+        """
+        This function is specific for the default architecture in nnU-Net. If you change the architecture, there are
+        chances you need to change this as well!
+        """
+        if self.is_ddp:
+            mod = self.network.module
+        else:
+            mod = self.network
+        if isinstance(mod, OptimizedModule):
+            mod = mod._orig_mod
+        
+        mod.deep_supervision = enabled
+
+
+class CMUNeXtTrainer_S(CMUNeXtTrainer):
+    @staticmethod
+    def build_network_architecture(architecture_class_name: str,
+                                   arch_init_kwargs: dict,
+                                   arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
+                                   num_input_channels: int,
+                                   num_output_channels: int,
+                                   patch_size: List[int],
+                                   enable_deep_supervision: bool = True) -> nn.Module:
+
+        return CMUNeXt(
+            input_channel=num_input_channels,
+            num_classes=num_output_channels,
+            dims=[8, 16, 32, 64, 128],
+            depths=[1, 1, 1, 1, 1],
+            kernels=[3, 3, 7, 7, 9]
+        )
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+
+class CMUNeXtBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, depth=1, k=3):
+        super(CMUNeXtBlock, self).__init__()
+        self.block = nn.Sequential(
+            *[nn.Sequential(
+                Residual(nn.Sequential(
+                    # deep wise
+                    nn.Conv2d(ch_in, ch_in, kernel_size=(k, k), groups=ch_in, padding=(k // 2, k // 2)),
+                    nn.GELU(),
+                    nn.BatchNorm2d(ch_in)
+                )),
+                nn.Conv2d(ch_in, ch_in * 4, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in * 4),
+                nn.Conv2d(ch_in * 4, ch_in, kernel_size=(1, 1)),
+                nn.GELU(),
+                nn.BatchNorm2d(ch_in)
+            ) for i in range(depth)]
+        )
+        self.up = conv_block(ch_in, ch_out)
+
+    def forward(self, x):
+        x = self.block(x)
+        x = self.up(x)
+        return x
+
+
+class conv_block(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(conv_block, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class up_conv(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(up_conv, self).__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.up(x)
+        return x
+
+
+class fusion_conv(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super(fusion_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, stride=1, padding=1, groups=2, bias=True),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_in),
+            nn.Conv2d(ch_in, ch_out * 4, kernel_size=(1, 1)),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_out * 4),
+            nn.Conv2d(ch_out * 4, ch_out, kernel_size=(1, 1)),
+            nn.GELU(),
+            nn.BatchNorm2d(ch_out)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class CMUNeXt(nn.Module):
+    def __init__(self, input_channel=3, num_classes=1, dims=[16, 32, 128, 160, 256], depths=[1, 1, 1, 3, 1], kernels=[3, 3, 7, 7, 7],
+                deep_supervision=True):
+        """
+        Args:
+            input_channel : input channel.
+            num_classes: output channel.
+            dims: length of channels
+            depths: length of cmunext blocks
+            kernels: kernal size of cmunext blocks
+        """
+        super(CMUNeXt, self).__init__()
+        # Encoder
+        self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.stem = conv_block(ch_in=input_channel, ch_out=dims[0])
+        self.encoder1 = CMUNeXtBlock(ch_in=dims[0], ch_out=dims[0], depth=depths[0], k=kernels[0])
+        self.encoder2 = CMUNeXtBlock(ch_in=dims[0], ch_out=dims[1], depth=depths[1], k=kernels[1])
+        self.encoder3 = CMUNeXtBlock(ch_in=dims[1], ch_out=dims[2], depth=depths[2], k=kernels[2])
+        self.encoder4 = CMUNeXtBlock(ch_in=dims[2], ch_out=dims[3], depth=depths[3], k=kernels[3])
+        self.encoder5 = CMUNeXtBlock(ch_in=dims[3], ch_out=dims[4], depth=depths[4], k=kernels[4])
+        # Decoder
+        self.Up5 = up_conv(ch_in=dims[4], ch_out=dims[3])
+        self.Up_conv5 = fusion_conv(ch_in=dims[3] * 2, ch_out=dims[3])
+        self.Up4 = up_conv(ch_in=dims[3], ch_out=dims[2])
+        self.Up_conv4 = fusion_conv(ch_in=dims[2] * 2, ch_out=dims[2])
+        self.Up3 = up_conv(ch_in=dims[2], ch_out=dims[1])
+        self.Up_conv3 = fusion_conv(ch_in=dims[1] * 2, ch_out=dims[1])
+        self.Up2 = up_conv(ch_in=dims[1], ch_out=dims[0])
+        self.Up_conv2 = fusion_conv(ch_in=dims[0] * 2, ch_out=dims[0])
+        self.Conv_1x1 = nn.Conv2d(dims[0], num_classes, kernel_size=1, stride=1, padding=0)
+        self.deep_supervision = deep_supervision
+        
+    def forward(self, x):
+        x1 = self.stem(x)
+        x1 = self.encoder1(x1)
+        x2 = self.Maxpool(x1)
+        x2 = self.encoder2(x2)
+        x3 = self.Maxpool(x2)
+        x3 = self.encoder3(x3)
+        x4 = self.Maxpool(x3)
+        x4 = self.encoder4(x4)
+        x5 = self.Maxpool(x4)
+        x5 = self.encoder5(x5)
+        d5 = self.Up5(x5)
+        d5 = torch.cat((x4, d5), dim=1)
+        d5 = self.Up_conv5(d5)
+        d4 = self.Up4(d5)
+        d4 = torch.cat((x3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+        d3 = self.Up3(d4)
+        d3 = torch.cat((x2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+        d2 = self.Up2(d3)
+        d2 = torch.cat((x1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+        d1 = self.Conv_1x1(d2)
+        if self.deep_supervision:
+            return [d1, d3, d4, d5]
+        else:
+            return d1
+
+
+def cmunext(dims=[16, 32, 128, 160, 256], depths=[1, 1, 1, 3, 1], kernels=[3, 3, 7, 7, 7]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
+
+
+def cmunext_s(dims=[8, 16, 32, 64, 128], depths=[1, 1, 1, 1, 1], kernels=[3, 3, 7, 7, 9]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
+
+
+def cmunext_l(dims=[32, 64, 128, 256, 512], depths=[1, 1, 1, 6, 3], kernels=[3, 3, 7, 7, 7]):
+    return CMUNeXt(dims=dims, depths=depths, kernels=kernels)
